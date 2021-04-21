6742d9d31693f1d461706801f427dfc9fd70b4aa
Aman Kumar <54955755+Arraxx@users.noreply.github.com>
2021-04-21 16:47:44 +0530

    Update Tasks.py

--- Tasks.py
+++ Tasks.py
@@ -5,9 +5,13 @@
 
 
 import streamlit as st
-import cv2
-from PIL import Image, ImageOps
 import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+import io
+import time
+from PIL import Image, ImageOps
+
 
 # In[10]:
 face = cv2.CascadeClassifier('C:/Users/kumar/Documents/intern/harsh.xml')
@@ -58,6 +62,40 @@ if result:
             break
     
     cp.release()
+
+result = st.button("Task 6")
+if result:
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1280)
+    cap.set(cv2.CAP_PROP_FPS, 30)
+
+    x, y, w, h = 800, 500, 100, 100
+    x, y, w, h = 950, 300, 100, 100
+    heartbeat_count = 128
+    heartbeat_values = [0]*heartbeat_count
+    heartbeat_times = [time.time()]*heartbeat_count
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    while(True):
+    
+        ret, frame = cap.read()    
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        crop_img = img[y:y + h, x:x + w]    
+        heartbeat_values = heartbeat_values[1:] + [np.average(crop_img)]
+        heartbeat_times = heartbeat_times[1:] + [time.time()]    
+        ax.plot(heartbeat_times, heartbeat_values)
+        fig.canvas.draw()
+        plot_img_np = np.fromstring(fig.canvas.tostring_rgb(),
+                                dtype=np.uint8, sep='')
+        plot_img_np = plot_img_np.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        plt.cla()    
+        cv2.imshow('Crop', crop_img)
+        cv2.imshow('Graph', plot_img_np)
+        k = cv2.waitKey(30) & 0xFF
+        if k == 27:
+            break
+cv2.destroyAllWindows()
 
 # In[ ]:
 
